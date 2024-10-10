c37a2ab7cd7c2d39e74c05ad09149c74098e810b
DucLV <tuvdhe172765@fpt.edu.vn>
2024-10-10 10:26:07 +0700

    t

---
+++ RazorMovies/RazorMovies/Data/RazorMoviesContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorMovies.Models;
+
+namespace RazorMovies.Data
+{
+    public class RazorMoviesContext : DbContext
+    {
+        public RazorMoviesContext (DbContextOptions<RazorMoviesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorMovies.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ RazorMovies/RazorMovies/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorMovies.Data;
+using RazorMovies.Models;
+
+namespace RazorMovies.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RazorMovies.Data.RazorMoviesContext _context;
+
+        public IndexModel(RazorMovies.Data.RazorMoviesContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movie.ToListAsync();
+        }
+    }
+}
